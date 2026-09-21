5544a2bdfa5e45a0682e7c86ce3318705fa4f81d
Nestor <vahid.fadaei.73@gmail.com>
2026-09-21 17:34:22 +0330

    Create src/modules/auth/auth.constants.ts

---
+++ src/modules/auth/auth.constants.ts
@@ -0,0 +1,11 @@
+export const AUTH_ACCESS_TOKEN_EXPIRES_IN =
+  "15m";
+
+export const AUTH_REFRESH_TOKEN_EXPIRES_IN =
+  "30d";
+
+export const AUTH_ACCESS_COOKIE =
+  "uniqe-access-token";
+
+export const AUTH_REFRESH_COOKIE =
+  "uniqe-refresh-token";

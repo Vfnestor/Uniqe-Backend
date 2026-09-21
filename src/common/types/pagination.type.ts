858a2a7f249830a7caa0d0105eb2934f9e9df74c
Nestor <vahid.fadaei.73@gmail.com>
2026-09-21 17:32:54 +0330

    Create src/common/types/pagination.type.ts

---
+++ src/common/types/pagination.type.ts
@@ -0,0 +1,13 @@
+export type PaginationMeta = {
+  page: number;
+  limit: number;
+  total: number;
+  totalPages: number;
+};
+
+export type PaginatedResponse<
+  T,
+> = {
+  items: T[];
+  meta: PaginationMeta;
+};

a23157aae97287a653991c7e1327801a6dd46903
Nestor <vahid.fadaei.73@gmail.com>
2026-09-21 17:28:39 +0330

    Create src/common/interceptors/response.interceptor.ts

---
+++ src/common/interceptors/response.interceptor.ts
@@ -0,0 +1,44 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from "@nestjs/common";
+
+import {
+  Observable,
+} from "rxjs";
+
+import {
+  map,
+} from "rxjs/operators";
+
+@Injectable()
+export class ResponseInterceptor<T>
+  implements NestInterceptor<
+    T,
+    {
+      success: boolean;
+      data: T;
+      timestamp: string;
+    }
+  >
+{
+  intercept(
+    _context: ExecutionContext,
+    next: CallHandler<T>,
+  ): Observable<{
+    success: boolean;
+    data: T;
+    timestamp: string;
+  }> {
+    return next.handle().pipe(
+      map((data) => ({
+        success: true,
+        data,
+        timestamp:
+          new Date().toISOString(),
+      })),
+    );
+  }
+}

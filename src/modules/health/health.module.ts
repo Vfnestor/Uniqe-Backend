c7f35608ff93bbd053e69792cf0a6276f3b8fd63
Nestor <vahid.fadaei.73@gmail.com>
2026-09-21 17:16:57 +0330

    Update health.module.ts

--- src/modules/health/health.module.ts
+++ src/modules/health/health.module.ts
@@ -6,9 +6,19 @@ import {
   HealthController,
 } from "./health.controller";
 
+import {
+  HealthService,
+} from "./health.service";
+
 @Module({
   controllers: [
     HealthController,
+  ],
+  providers: [
+    HealthService,
+  ],
+  exports: [
+    HealthService,
   ],
 })
 export class HealthModule {}

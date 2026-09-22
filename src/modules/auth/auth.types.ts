45ea4cbf6c45125501fa1ff865375311e0ac85c5
Nestor <vahid.fadaei.73@gmail.com>
2026-09-22 11:53:01 +0330

    Update auth.types.ts

--- src/modules/auth/auth.types.ts
+++ src/modules/auth/auth.types.ts
@@ -7,12 +7,22 @@ export type AuthTokens = {
   refreshToken: string;
 };
 
-export type AuthenticatedUser = Omit<
-  User,
-  "password"
->;
+export type AuthenticatedUser =
+  Omit<User, "password">;
 
 export type AuthResult = {
   user: AuthenticatedUser;
   tokens: AuthTokens;
+};
+
+export type JwtAccessPayload = {
+  sub: string;
+  email: string;
+  role: string;
+  type: "access";
+};
+
+export type JwtRefreshPayload = {
+  sub: string;
+  type: "refresh";
 };

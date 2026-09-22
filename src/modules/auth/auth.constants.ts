cd52dfc0d297210479f9b8b731bb8de4543ac6a9
Nestor <vahid.fadaei.73@gmail.com>
2026-09-22 12:30:34 +0330

    Update auth.constants.ts

--- src/modules/auth/auth.constants.ts
+++ src/modules/auth/auth.constants.ts
@@ -4,6 +4,9 @@ export const AUTH_ACCESS_TOKEN_EXPIRES_IN =
 export const AUTH_REFRESH_TOKEN_EXPIRES_IN =
   "30d" as const;
 
+export const AUTH_REFRESH_TOKEN_DAYS =
+  30;
+
 export const AUTH_ACCESS_COOKIE =
   "uniqe-access-token";
 
